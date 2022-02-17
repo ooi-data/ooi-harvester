02f92c0b2a8864ccda601b3847a55330feb999b4
lsetiawan <landungs@uw.edu>
2022-02-16 17:17:24 -0800

    Add a way to register flow when code changes

---
+++ .ci/register-flow.py
@@ -0,0 +1,41 @@
+import datetime
+import os
+from pathlib import Path
+from prefect.storage.docker import Docker
+from ooi_harvester.pipelines.stream.flow import create_flow
+
+HERE = Path(__file__).resolve().parent
+
+
+def main():
+    now = datetime.datetime.utcnow()
+    image_registry = "cormorack"
+    image_name = "ooi-harvester"
+    project_name = os.environ.get("PREFECT_PROJECT", "ooi-harvest")
+    image_tag = f"{now:%Y%m%dT%H%M}"
+
+    docker_storage = Docker(
+        registry_url=image_registry,
+        dockerfile=HERE.joinpath("Dockerfile"),
+        image_name=image_name,
+        prefect_directory="/home/jovyan/prefect",
+        env_vars={'HARVEST_ENV': 'ooi-harvester'},
+        python_dependencies=[
+            'git+https://github.com/ooi-data/ooi-harvester.git@main'
+        ],
+        image_tag=image_tag,
+        build_kwargs={
+            'nocache': True,
+            'buildargs': {
+                'PYTHON_VERSION': os.environ.get('PYTHON_VERSION', 3.8)
+            },
+        },
+    )
+
+    flow = create_flow(storage=docker_storage)
+    flow.validate()
+    flow.register(project_name=project_name)
+
+
+if __name__ == "__main__":
+    main()
